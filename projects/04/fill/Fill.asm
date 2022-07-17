8f02c7a37ceb183d459a86dba652dedf299d2aae
Robledo Pazotto <robledo@gmail.com>
2022-07-16 19:46:49 -0700

    .

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(KEYBOARD_INPUT)
+    @KBD
+    D=M
+    @PRINT_BLACK
+    D;JNE
+
+(PRINT_WHITE)
+    @SCREEN
+    D=A
+    @addr
+    M=D                // addr = 16384
+                       // (screen's base address)
+    @8191
+    D=A
+    @n
+    M=D               // n = RAM[0]
+
+    @i
+    M=0               // i = 0
+
+(LOOP_WHITE)
+    @i
+    D=M
+    @n
+    D=D-M
+    @KEYBOARD_INPUT
+    D;JGT             // if i > n goto END
+
+    @addr
+    A=M
+    M=0              // RAM[addr]=0
+
+    @i
+    M=M+1             // i = i + 1
+    @1
+    D=A
+    @addr
+    M=D+M             
+    @LOOP_WHITE
+    0;JMP             // goto LOOP_WHITE
+
+(PRINT_BLACK)
+    @SCREEN
+    D=A
+    @addr
+    M=D                // addr = 16384
+                       // (screen's base address)
+    @8191
+    D=A
+    @n
+    M=D               // n = RAM[0]
+
+    @i
+    M=0               // i = 0
+
+(LOOP_BLACK)
+    @i
+    D=M
+    @n
+    D=D-M
+    @KEYBOARD_INPUT
+    D;JGT             // if i > n goto END
+
+    @addr
+    A=M
+    M=-1              // RAM[addr]=1111111111111111
+
+    @i
+    M=M+1             // i = i + 1
+    @1
+    D=A
+    @addr
+    M=D+M             // addr = addr + 32
+    @LOOP_BLACK
+    0;JMP             // goto LOOP
